6d199eac68aa8e0a2a598ae65b72273859699f76
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-22 15:31:27 +0800

    Added variable for running program

--- selection_sort.py
+++ selection_sort.py
@@ -6,4 +6,11 @@ def sort(nums):
             if nums[j] < nums[i]:
                 minpos = j
 
-     
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+nums = [11, 21, 1, 73, 32, 24, 90, 75, 6, 47]
+sort(nums)
+
+print(nums)
