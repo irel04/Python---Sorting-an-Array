61381e3a8fe06dd1a1cc91ff22f0d75208e0103d
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-23 14:12:24 +0800

    Program complete

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,15 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
-        key = array[i]
-        if array[i]:
-            pass
+        anchor = array[i]
+        j = i - 1
+        
+        while j >= 0 and anchor < array[j]:
+            array[j+1] = array[j]
+            j = j - 1
+        array[j+1] = anchor
+
+
+my_array = [11, 21, 1, 73, 32, 24, 90, 75, 6, 47]
+insertion_sort(my_array)
+print(my_array)
         
