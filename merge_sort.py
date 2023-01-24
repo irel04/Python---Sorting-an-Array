d4f4e3c0bf1792b5f7555559a778130a64a2b883
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-24 12:19:51 +0800

    Merge sort alogrithm implementation on code

--- merge_sort.py
+++ merge_sort.py
@@ -1,1 +1,13 @@
 my_array = [11, 21, 1, 73, 32, 24, 90, 75, 6, 47]
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return
+    
+    mid = len(array)//2
+
+    left = array[:mid]
+    right = array[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
