bdd71605c3aa41d47f34c8390d3837b38430ca19
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-24 12:54:38 +0800

    Added block of codes in function partition

--- quick_sort.py
+++ quick_sort.py
@@ -15,3 +15,17 @@ def quick_sort(elements, start, end):
 def partition(elements, start, end):
     pivot_index = start
     pivot = elements[pivot_index]
+   
+    while start < end:
+        while start < len(elements) and elements[start] <= pivot:
+            start+=1
+
+        while elements[end] > pivot:
+            end-=1
+
+        if start < end:
+            swap(start, end, elements)
+
+    swap(pivot_index, end, elements)
+
+    return end
