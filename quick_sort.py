2741121039a1905b591b8c8a44a11c66df5552fe
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-24 12:53:28 +0800

    partioning function was created

--- quick_sort.py
+++ quick_sort.py
@@ -11,3 +11,7 @@ def quick_sort(elements, start, end):
         pi = partition(elements, start, end)
         quick_sort(elements, start, pi-1)
         quick_sort(elements, pi+1, end)
+
+def partition(elements, start, end):
+    pivot_index = start
+    pivot = elements[pivot_index]
