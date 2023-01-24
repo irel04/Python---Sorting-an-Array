6078f4c193e1d7af63ffbfd8237b5e8b6818da40
Mark Lhester Agellon <kianirel56@gmail.com>
2023-01-24 12:52:38 +0800

    quick sort function was created

--- quick_sort.py
+++ quick_sort.py
@@ -5,3 +5,9 @@ def swap(a, b, arr):
         tmp = arr[a]
         arr[a] = arr[b]
         arr[b] = tmp
+
+def quick_sort(elements, start, end):
+    if start < end:
+        pi = partition(elements, start, end)
+        quick_sort(elements, start, pi-1)
+        quick_sort(elements, pi+1, end)
